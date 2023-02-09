1457a2419b4685515f262215bc05d0166877cd61
opensnztech <fostin.opensnz@gmail.com>
2023-02-09 15:51:40 +0000

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
 #include "main.h"
+#include "Storage.h"
+#include "web.h"
 #include "gateway.h"
 #include "transceiver.h"
 #include "forwarder.h"
@@ -15,17 +17,23 @@ TaskHandle_t hFGateway    = NULL;
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    WiFi.begin("OPPO", "Fostin2022");
-    xTaskCreatePinnedToCore(TransceiverTaskEntry, "transceiverTask",  10000, NULL, 1, &hTransceiver, 0);            
-    xTaskCreatePinnedToCore(ForwarderTaskEntry,   "forwarderTask",    10000, NULL, 1, &hForwarder, 1);
-    // Wait to Launch transceiverTask and forwarderTask
+    while(!Storage.begin())
+    {
+        delay(10);
+    }
+    Web.begin();
     delay(100);
-    xTaskCreatePinnedToCore(TGatewayTaskEntry, "tGatewayTask",  10000, NULL, 1, &hTGateway, 0);            
+    xTaskCreatePinnedToCore(TGatewayTaskEntry, "tGatewayTask",  10000, NULL, 1, &hTGateway, 0);
+    delay(100);          
     xTaskCreatePinnedToCore(FGatewayTaskEntry, "fGatewayTask",  10000, NULL, 1, &hFGateway, 1);
+    delay(100);
+    xTaskCreatePinnedToCore(TransceiverTaskEntry, "transceiverTask",  10000, NULL, 1, &hTransceiver, 0);
+    delay(100);           
+    xTaskCreatePinnedToCore(ForwarderTaskEntry,   "forwarderTask",    10000, NULL, 1, &hForwarder, 1);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    // Sleep indefinitely
     delay(portMAX_DELAY);
 }
 
@@ -35,26 +43,22 @@ void loop() {
 /*********************** Task Entry ************************/
 
 void TGatewayTaskEntry(void * parameter){
-    delay(100);
-    Serial.println("TGatewayTaskEntry");
+    APP_PRINT_LN("TGatewayTaskEntry");
     Gateway.tMain();
 }
 
 void FGatewayTaskEntry(void * parameter){
-    delay(200);
-    Serial.println("FGatewayTaskEntry");
+    APP_PRINT_LN("FGatewayTaskEntry");
     Gateway.fMain();
 }
 
 void TransceiverTaskEntry(void * parameter){
-    delay(10);
-    Serial.println("TransceiverTaskEntry");
+    APP_PRINT_LN("TransceiverTaskEntry");
     Transceiver.main();
 }
 
 void ForwarderTaskEntry(void * parameter){
-    delay(20);
-    Serial.println("ForwarderTaskEntry");
+    APP_PRINT_LN("ForwarderTaskEntry");
     Forwarder.main();
 }
 
