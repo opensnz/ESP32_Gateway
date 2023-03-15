449a651fdeea3750b774659206d9111796e8dcae
khalidopensnz <khalid.opensnz@gmail.com>
2023-03-15 12:20:43 +0100

    up

--- data/script.js
+++ data/script.js
@@ -6,29 +6,6 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 
-function menuOnClick(btn)
-{
-    var menu = document.getElementsByClassName("menu");
-    for(var i=0; i<menu.length; i++)
-    {
-        menu[i].classList.remove('selected');
-    }
-    btn.classList.add("selected");
-    switch(btn.id)
-    {
-        case "home":
-            home();
-            break;
-        case "device":
-            device();
-            break;
-        case "network":
-            network();
-            break;
-    }
-}
-
-
 function home()
 {
     console.log("home menu");
@@ -45,3 +22,30 @@ function network()
 {
     console.log("network menu");
 }
+
+
+function menuOnClick(target) {
+  // Get the target section's top offset
+  const offsetTop = document.querySelector(target).offsetTop;
+
+  // Scroll to the target section
+  window.scrollTo({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
+function getSystemInfo() {
+  fetch('/system')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('flash-freq').value = data.flash.freq;
+      document.getElementById('flash-size').value = data.flash.size;
+      document.getElementById('heap-size').value = data.heap.size;
+      document.getElementById('heap-free').value = data.heap.free;
+      document.getElementById('disk-size').value = data.disk.size;
+      document.getElementById('disk-used').value = data.disk.used;
+    })
+    .catch(error => console.error(error));
+}
+
